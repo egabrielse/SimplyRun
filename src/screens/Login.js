b143431646af48a7e027779eb75c05f1503409e5
egabrielse <egabrielse11@gmail.com>
2020-03-10 22:02:59 -0500

    Added Screens and installed redux & react-redux

---
+++ src/screens/Login.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react';
+import { Text, View, TextInput, Button, Alert  } from 'react-native';
+import firebase from 'firebase';
+import Firebase from '../firebase/Firebase.js'
+import { connect } from 'react-redux'
+
+//References to the root of the firestore database
+const firestore = firebase.firestore();
+//Firebase initialzation 
+Firebase
+
+//Login Screen 
+class LoginScreen extends Component {
+    //Using the Firebase built in authentification to login 
+    login = () => {
+        if (this.props.email != null && this.props.password != null &&
+            this.props.email.trim() != "" && this.props.password.trim() != "") {
+            firebase
+                .auth()
+                .signInWithEmailAndPassword(this.props.email, this.props.password)
+                .then(() => this.props.navigation.navigate('Run'))
+                .catch(error => Alert.alert(error.message));
+        }
+    }
+    //Function to change to the Create Acount Screen
+    goToCreateAccount = () => {
+        this.props.navigation.navigate('CreateAccount');
+    }
+
+    //Basic Funtion that sends some hard coded data to a specific place in the 
+    //firestore database
+    sendToFirebase = () => {
+        firestore.collection('users').doc('test').set({
+            name: "SimpleTest",
+            weight: "260",
+            height: "6"
+        })
+    }
+    //Basic function that retrieves some date from firestore and displays it on the console
+    getFromFirebase = () => {
+        var ref = firestore.collection('users').doc('test');
+        ref.get().then(testData => {
+            console.log(testData.data())
+        })
+    }
+
+    render() {
+        return (
+            <View style={{ flex: 1, alignContent: 'center', justifyContent: 'center' }} >
+                <Text>Login</Text>
+                <TextInput
+                    placeholder="Email"
+                    autoCapitalize="none"
+                    onChangeText={email => this.props.typeEmail(email)}
+                    value={this.props.email}
+                />
+                <TextInput
+                    secureTextEntry
+                    placeholder="Password"
+                    autoCapitalize="none"
+                    onChangeText={password => this.props.typePassword(password)}
+                    value={this.props.password}
+                />
+                <Button title="Login" onPress={this.login} />
+                <Button title="CreateAccount" onPress={this.goToCreateAccount} />
+                <Text>This sends some hardcoded data to the firestore database</Text>
+                <Button title="Write To Firebase" onPress={this.sendToFirebase} />
+                <Text>This retrieves some data from firestore and prints it on the console </Text>
+                <Button title="Read From Firebase" onPress={this.getFromFirebase} />
+
+            </View>
+        );
+    }
+    
+}
+//Getting the states from the store and mapping it to props in the LoginScreen
+function mapStateToProps(state) {
+    return {
+        email: state.email,
+        password: state.password
+    }
+}
+//Sends actions to the reducer in the App.js file 
+function mapDispatchtoProps(dispatch) {
+    return {
+        typeEmail: (email) => dispatch({ type: "EMAIL", email }),
+        typePassword: (password) => dispatch({ type: "PASSWORD", password })
+    }
+}
+
+//Connecting the react components to the store in App.js 
+export default connect(mapStateToProps, mapDispatchtoProps)(LoginScreen);
---
+++ src/screens/SimplyRun.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import firebase from '../config/firebase'
+
+export default function SimplyRun ({ route, navigation}) {
+        return (
+            <View >
+                <Text></Text>
+            </View>
+        );
+    
+}
