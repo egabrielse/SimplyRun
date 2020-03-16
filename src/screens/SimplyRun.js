3e2d6c21325285781ca16cdc5285e88ebda417f8
sampeaslee <speaslee@wisc.edu>
2020-03-15 19:07:23 -0500

    added start button

--- src/screens/SimplyRun.js
+++ src/screens/SimplyRun.js
@@ -1,16 +1,77 @@
-import React, { Component } from 'react';
-import { Text, View, TextInput, Button } from 'react-native';
+import React, { Component, } from 'react';
+import { Text, View, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import firebaseConfig from '../config/firebaseConfig'
 
+class Triangle extends Component {
+
+    render() {
+        return (
+            <View style={{
+                width: 0,
+                height: 0,
+                backgroundColor: 'transparent',
+                borderStyle: 'solid',
+                borderLeftWidth: 35,
+                borderRightWidth: 35,
+                borderBottomWidth: 70,
+                borderLeftColor: 'transparent',
+                borderRightColor: 'transparent',
+                borderBottomColor: 'steelblue',
+                transform: [{ rotate: '90deg' }]
+
+            }} />
+        )
+    }
+}
+class StartButton extends Component {
+    render() {
+        return (
+            <TouchableOpacity
+                style={{
+                    borderWidth: 1,
+                    borderColor: 'rgba(0,0,0,0.2)',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    width: 150,
+                    height: 150,
+                    backgroundColor: 'springgreen',
+                    borderRadius: 75
+                }} onPress={this.props.onPress} >
+                <Triangle />
+            </TouchableOpacity>
+        )
+    }
+}
+
 export default class SimplyRun extends Component {
-  render() {
-    return (
-      <ScrollView >
-        <View>
-          <Text > Simply Run </Text>
-        </View>
-      </ScrollView>
-    );
-  }
+    state = {
+        stats: 'Time: \n Ditance: \n Pace: \n calories:',
+        displayStat: false,
+        time: 0
+    }
+
+    displayStats = () => {
+        if (!this.state.displayStat) {
+            this.setState({ displayStat: true })
+        } else {
+            this.setState({ displayStat: false })
+        }
+    }
+
+
+
+    render() {
+        ``
+        return (
+
+            <View style={{ flex: 1, justifyContent: 'flex-end', alignItems: 'center', padding: 75 }}>
+                {
+                    this.state.displayStat ? < Text style={{ paddingVertical: 100 }}> {this.state.stats}</Text> : null
+                }
+                <StartButton onPress={this.displayStats} />
+            </View>
+
+        );
+    }
 }
