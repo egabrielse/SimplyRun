c2da1f3fa7859ab3c7882940baf36829c30de078
Peter Newman <pnewman2@wisc.edu>
2020-03-21 00:10:05 -0500

    added basic table displaying runs, start modal work

--- src/screens/RunLog.js
+++ src/screens/RunLog.js
@@ -1,38 +1,141 @@
 import React, { Component } from 'react';
-import { Text, View, TextInput, Button } from 'react-native';
+import { Modal, Text, TouchableOpacity, View, TextInput, Button, StyleSheet, TouchableHighlight, Alert } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
+import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
+import { connect } from 'react-redux';
 import firebaseConfig from '../config/firebaseConfig'
 import firebase from 'firebase';
 
-export default class RunLog extends Component {
+class RunLog extends Component {
 
 	state = {
-        user: {
-			"name":"Jon Doe",
-			"weight": 182
-		}
-    }
+        tableHead: ['Date', 'Distance', 'Time', 'Pace'],
+        tableData: [],
+        modalVisible: false,
+        modalData: []
+    };
 
-	// GetUserData() {
-	// 	var jondoe = firebaseConfig.Firestore().collection('users').doc(user.uid)
-	// }
+	async componentDidMount() {
 
-	componentDidMount() {
-        var testrun = firestore.collection('users').doc('0yjmOiXjzCS74wmLW8l3EFHJYj83')
-        						.collection('runs').doc('Vj8iAkbyUKZgB2f4I4MD');
-    }
+      //get all runs in runlog and populate table
+      const tableData = []
+      await firebaseConfig.firestore().collection("users").doc(firebaseConfig.auth().currentUser.uid)
+                                      .collection("RunLog").get().then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+            const rowData = [];
+            rowData.push(doc.get("start_time").toDate().toString());  //fix this formatting
+            rowData.push(doc.get("distance"));
+            rowData.push(doc.get("time"));
+            rowData.push(doc.get("time"));
+            // rowData.push(
+            //   <TouchableHighlight onPress={() => this.setModalVisible(true, doc.getId())}>
+            //     <View style={styles.btn}>
+            //       <Text style={styles.btnText}>button</Text>
+            //     </View>
+            //   </TouchableHighlight>
+            // )
+            tableData.push(rowData);
+        });
+      });
+
+    this.setState({
+      tableData : tableData
+    });
+
+  }
+
+  setModalVisible(visible) {
+    this.setState({modalVisible: visible});
+    this.RunDetails();
+  //  alert("Id is" + id);
+  }
+
+  async RunDetails() {
+    var id = "RqEr89LjBrxJE4l1gk6Y";
+
+          //get info for specific run
+          const modalData = []
+          await firebaseConfig.firestore().collection("users").doc(firebaseConfig.auth().currentUser.uid)
+                                          .collection("RunLog").doc(id).get().then(function(doc) {
+                const rowData = [];
+                modalData.push("Time: " + doc.get("time") + "\n");  //fix this formatting
+                modalData.push("Distance: " + doc.get("distance") + "\n");
+                modalData.push("Pace: 7:22 min/mi" + "\n");
+                modalData.push("Pace: " + doc.get("calories") + "\n");
+                modalData.push("Notes: " + doc.get("note") + "\n");
+          });
+    
+        this.setState({
+          modalData : modalData
+        });
+
+  }
+
 
   render() {
     return (
       <ScrollView >
         <View>
-          <Text > Run Log for {this.props.user} </Text>
+          <Text > Run Log for {this.name} </Text>
           <Text > Here is a list of all the beautiful runs you have been on! </Text>
-          <Text > Run 1: 4 miles </Text>
-          <Text > Run 2: 3.6 miles </Text>
+          <Table borderStyle={{borderWidth: 2, borderColor: '#c8e1ff'}}>
+            <Row data={this.state.tableHead} style={styles.head} textStyle={styles.text}/>
+            <Rows data={this.state.tableData} textStyle={styles.text}/>
+          </Table>
+
+          <TouchableHighlight onPress={() => this.setModalVisible(!this.state.modalVisible)}>
+                <View style={styles.btn}>
+                  <Text style={styles.btnText}>button</Text>
+                </View>
+          </TouchableHighlight>
+
+        <Modal
+          animationType="slide"
+          transparent={false}
+          visible={this.state.modalVisible}
+          onRequestClose={() => {
+            Alert.alert('Modal has been closed.');
+          }}>
+          <View style={{marginTop: 22}}>
+            <View>
+            <TouchableHighlight
+                onPress={() => {
+                  this.setModalVisible(!this.state.modalVisible);
+                }}>
+                <Text>Back</Text>
+              </TouchableHighlight>
+
+              <Text>Run Details</Text>
+              <Text>{this.state.modalData}</Text>
+
+
+
+              
+            </View>
+          </View>
+        </Modal>
 
         </View>
       </ScrollView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#fff' },
+  head: { height: 40, backgroundColor: '#f1f8ff' },
+  text: { margin: 6 }
+});
+
+// const mapDispatchToProps = dispatch => {
+// //  return bindActionCreators({ updateEmail, updatePassword, updateName, updateFavoriteThings, update }, dispatch)
+// }
+
+const mapStateToProps = state => {
+  return {
+      user: state.user
+  }
+}
+
+//Connecting the react components to the store in App.js 
+export default connect(mapStateToProps)(RunLog);
