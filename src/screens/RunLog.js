f4f32dac4220f4bf911fda4680475a5b64525250
Peter Newman <pnewman2@wisc.edu>
2020-04-13 15:24:35 -0500

    rounded time to nearest second

--- src/screens/RunLog.js
+++ src/screens/RunLog.js
@@ -28,7 +28,7 @@ class RunLog extends Component {
     var hours = Math.trunc(time / 3600);
     time %= 3600;
     var minutes = Math.trunc(time / 60);
-    var seconds = (time % 60).toFixed(2);
+    var seconds = (time % 60).toFixed();
     if (hours   < 10) hours   = "0" + hours;
     if (minutes < 10) minutes = "0" + minutes;
     if (seconds < 10) seconds = "0" + seconds;
