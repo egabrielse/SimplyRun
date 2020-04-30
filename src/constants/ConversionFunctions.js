fd9275da463f80a91e4bb94e46ebf72a5604d9a7
egabrielse <egabrielse11@gmail.com>
2020-04-29 20:16:27 -0500

    added back the removed functions

--- src/constants/ConversionFunctions.js
+++ src/constants/ConversionFunctions.js
@@ -1,10 +1,22 @@
 const HEIGHT_RATIO = 0.393701;
 const WEIGHT_RATIO = 0.453592;
 
+export const convertInchesToCentimeters = (inches) => {
+    // cm = in / 0.393701
+    console.log("ConversionFunctions: convertInchesToCentimeters ( inches=", inches,")")
+    return Math.round(inches / HEIGHT_RATIO)
+}
+
 export const convertCentimetersToInches = (centimeters) => {
     // in = cm * 0.393701
     console.log("ConversionFunctions: convertCentimetersToInches ( inches=", centimeters,")")
     return Math.round(centimeters * HEIGHT_RATIO) 
+}
+
+export const convertPoundsToKilograms = (pounds) => {
+    // kg = lb * 0.453592
+    console.log("ConversionFunctions: convertPoundsToKilograms ( pounds=", pounds,")")
+    return Math.round(pounds * WEIGHT_RATIO)
 }
 
 export const convertKilogramsToPounds = (kilograms) => {
