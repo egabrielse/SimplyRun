5bb4bec2d763d839b4e5d52000c4b172022237ad
pnewman97 <35820310+pnewman97@users.noreply.github.com>
2020-04-30 19:45:59 -0500

    Runlog (#28)
    * put some test stuff on runlog screen
    
    * trying to display run subcollection for user
    
    * added basic table displaying runs, start modal work
    
    * modified tabnavigator to include run log
    
    * added modal for rundetails, display route
    
    * some css changes
    
    * formatting for time and pace
    
    * clickable rows, css updates
    
    * Login create screens (#11)
    
    * Login/CreateAccount_Screens
    
    * Created Login, AccountCreate, and PersonalInfo screens
    
    * Changed PersonalInfoReducer and created SettingsReducer
    
    * Login Screen now fetches data from firestore
    
    Fixed account creation to store only imperial vals in database (#13)
    
    Settings screen (#14)
    
    * basic setup
    
    * formatting updates
    
    * added navigation to settings
    
    * progress, but can't get stupid refresh to work
    
    * disabled yellow box to fix updating issue
    
    * adapting to create account format
    
    * Unstyled but functional update
    
    * added some styling to settings
    
    * basic setup
    
    formatting updates
    
    added navigation to settings
    
    progress, but can't get stupid refresh to work
    
    disabled yellow box to fix updating issue
    
    adapting to create account format
    
    Unstyled but functional update
    
    added some styling to settings
    
    * reworked to use redux. Not perfect yet
    
    * saving work
    
    * saving more changes
    
    * basic setup
    
    * formatting updates
    
    * added navigation to settings
    
    * progress, but can't get stupid refresh to work
    
    * disabled yellow box to fix updating issue
    
    * adapting to create account format
    
    * Unstyled but functional update
    
    * added some styling to settings
    
    * reworked to use redux. Not perfect yet
    
    * saving work
    
    * saving more changes
    
    * settings works, still not quite there with edit
    
    * editSettings works, added a few comments
    
    * made suggested updates
    
    * added logout and delete account functionalities
    
    * Got rid of old yellow box fix. Edited a couple comments
    
    put some test stuff on runlog screen
    
    trying to display run subcollection for user
    
    added basic table displaying runs, start modal work
    
    modified tabnavigator to include run log
    
    added modal for rundetails, display route
    
    some css changes
    
    formatting for time and pace
    
    clickable rows, css updates
    
    fixed a few warnings and bugs
    
    * added sorting for table, added ability to delete a run
    
    * simplified RunLogAction a bit
    
    * fixed issue with empty route causing error
    
    * rounded time to nearest second
    
    * fixed date to pass tests, changed the way pace is displayed
    
    * formatting for calories
    
    * added more tests to runlog
    
    * more tests
    
    * slight refactor of runlog screen, more code coverage
    
    Co-authored-by: egabrielse <39629114+egabrielse@users.noreply.github.com>

--- __tests__/RunLog.test.js
+++ __tests__/RunLog.test.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ConnectedRunLog, {RunLog} from '../src/screens/RunLog';
 import renderer from 'react-test-renderer';
 import configureStore from 'redux-mock-store'
+import populatedRunsState from '../__mock_stores__/populatedRunsState'
 import initialState from '../__mock_stores__/initialState'
 import { Provider } from 'react-redux';
-import Enzyme, { shallow } from 'enzyme'
+import Enzyme, { shallow, mount } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
+import { Alert } from 'react-native';
+import firebase from 'firebase';
 
 const createMockStore = configureStore([])
 
@@ -51,18 +54,134 @@ jest.mock('react-native-maps', () => {
 });
 
 
+const run = [{
+    id: "testID",
+    note: "well done Jon",
+    time: 720,
+    distance: 2.1,
+    pace: 5.71428571,
+    calories: 0,
+    start_time: new Date("2020-03-22T12:48:54Z"),
+    end_time: new Date("2020-03-22T13:00:54Z"),
+    route: [new firebase.firestore.GeoPoint(43.073051,-89.40123), new firebase.firestore.GeoPoint(43.073451,-89.40123)],
+}]
+
+const run2 = {
+    id: "testID2",
+    note: "well done again, Jon",
+    time: 720,
+    distance: 2.1,
+    pace: 5.71428571,
+    calories: 0,
+    start_time: new Date("2020-03-22T12:48:54Z"),
+    end_time: new Date("2020-03-22T13:00:54Z"),
+    route: [new firebase.firestore.GeoPoint(43.073051,-89.40123), new firebase.firestore.GeoPoint(43.073451,-89.40123)],
+}
+
 describe("RunLog screen", () => {
 
     beforeEach(() => {
-        mockStore = createMockStore(initialState)
+        mockStore = createMockStore(populatedRunsState)
     })
 
     it('renders correctly', () => {
         const tree = renderer.create(
-            <Provider store={mockStore}>
+            <Provider store={createMockStore(initialState)}>
                 <ConnectedRunLog/>
             </Provider>
         ).toJSON();
         expect(tree).toMatchSnapshot();
     });
+
+    test('open modal', () => {
+        const navigation = jest.mock();
+        navigation.navigate = jest.fn();
+        const tree = renderer.create(
+            <Provider store={createMockStore(populatedRunsState)}>
+                <ConnectedRunLog/>
+            </Provider>
+        ).toJSON();
+        const enzymeWrapper = shallow(<RunLog store={createMockStore(populatedRunsState)} navigation={navigation}/>)
+        const component = enzymeWrapper.dive().dive().dive();
+        component.find("TouchableHighlight").at(0).props().onPress()
+        component.find("Cell").at(0).props().onPress()
+        component.find("TouchableHighlight").at(1).props().onPress()
+        
+        expect(navigation.navigate).not.toHaveBeenCalled();
+    })
+
+
+    test("Test RunDetails", () => {
+        
+        const t = renderer.create(<RunLog runs={run} />)
+        const instance = t.getInstance()
+//        instance.componentDidMount()
+        instance.RunDetails("blah")
+        expect(instance.state.selectedRun).toBe(null)
+        instance.RunDetails("testID")
+        
+        expect(instance.state.selectedRun.id).toBe("testID")
+    
+    })
+    
+    test("Test DeleteRun", () => {
+        Alert.alert = jest.fn()
+        const t = renderer.create(<RunLog />)
+        const instance = t.getInstance()
+        instance.ConfirmDeleteRun()
+        expect(Alert.alert).toHaveBeenCalled()
+    
+    })
+    
+    test("Test formatStats", () => {
+        const t = renderer.create(<RunLog runs={run} />)
+        const instance = t.getInstance()
+        instance.componentDidMount()
+//        const result = instance.formatStats(instance.state.tableData[0])
+        instance.sortTable(0, true)
+        instance.sortTable(1, true)
+        instance.sortTable(2, true)
+        instance.sortTable(3, true)
+        instance.setModalVisible(true, "testID")
+        instance.props.runs.push(run2);
+        instance.componentDidUpdate()
+//        expect(result[0]).toBe("3/22/2020")
+    
+    
+    })
+
+    // describe('Modal', () => {
+    //     it('renders the Modal Correctly', () => {
+    //       const { modal } = render(<TextAreaInput {…props} />)
+          
+    //       getByText(props.labelText)
+    //     })
+    //   })
+    
 })
+
+// test("Test formatTime", () => {
+//     const t = renderer.create(<RunLog />)
+//     const instance = t.getInstance()
+//     const time = instance.formatTime(123)
+//     expect(time).toBe("00:02:03")
+
+// })
+
+// test("Test formatDate", () => {
+//     const t = renderer.create(<RunLog />)
+//     const instance = t.getInstance()
+//     const date = new Date("October 13, 2013 11:13:00")
+//     const formatted = instance.formatDate(date)
+//     expect(formatted).toBe("10/13/2013")
+
+// })
+
+// test("Test formatPace", () => {
+//     const t = renderer.create(<RunLog />)
+//     const instance = t.getInstance()
+//     const pace = instance.formatPace(12.5)
+//     expect(pace).toBe("12:30")
+
+// })
+
